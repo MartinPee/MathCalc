6971daf3c3393a0c0273a85f60062a84d3bd4494
marci <marcinpikul87@gmail.com>
2018-08-21 16:43:22 +0200

    Content class

--- main/java/MathCalc/CalculateHelper.java
+++ main/java/MathCalc/CalculateHelper.java
@@ -1,4 +1,19 @@
 package MathCalc;
 
 public class CalculateHelper {
+
+    MathCommand command;
+    double leftValue;
+    double rightValue;
+    double result;
+
+    public void process(String statement) {
+        // add 1.0 2.0
+        String[] parts = statement.split (" ");
+        String comandString = parts[0]; // add
+        leftValue = Double.parseDouble(parts[1]); // 1.0
+        rightValue = Double.parseDouble(parts[2]); // 2.0
+    }
+
+    private void
 }
