926b9feedf6870e0161f587c5e7c0fc02bd779c6
marci <marcinpikul87@gmail.com>
2018-09-03 23:45:14 +0200

    New features

--- main/java/MathCalc/Main.java
+++ main/java/MathCalc/Main.java
@@ -8,11 +8,21 @@ public class Main {
     public static void main(String[] args) {
 
         String[] statements = {
+                "add 1.0",
+                "add xx 25.0",
+                "addX 0.0.0.0",
                 "divide 100.0 50.0",
                 "add 25.0 92.0",
                 "substract 225.0 17.0",
                 "multiply 11.0 3.0"
         };
+
+            CalculateHelper helper = new CalculateHelper();
+            for ( String statement: statements){
+                helper.process(statement);
+                System.out.println(helper);
+            }
+
 
         MathEquation[] equations = new MathEquation[4];
         equations[0] = new MathEquation('d', 100.0d, 50.0d);
